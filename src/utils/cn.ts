ff1dc4631504e8201402673695aefdf72c1a97eb
XpertWebApp <appweb.live@gmail.com>
2026-09-01 09:31:01 +0000

    Sync Flight Builder (develop @ 4a1383b)

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -4,6 +4,7 @@
  *
  *   cn('btn', isActive && 'btn-active', error ? 'btn-error' : null)
  */
+
 export function cn(...classes: Array<string | false | null | undefined>): string {
   return classes.filter(Boolean).join(' ')
 }
